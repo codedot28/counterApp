c999722598c0bf79e1cd6d11bd60f28905dc4f86
jangkyum <codedot28@gmail.com>
2024-07-31 18:18:24 +0900

    axios add

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import axios from '../../api/axios';
 
 export const Contact: React.FC = () => {
-    return<div>contact</div>
+  const [product, setProduct] = useState<any>(null);
+
+  async function getProduct() {
+    try {
+      const res = await axios.get('/posts/1');
+      console.log(res);
+      const nextProduct = res.data;
+      setProduct(nextProduct);
+    } catch (error) {
+      console.error("Error fetching the product:", error);
+    }
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+
+    return(
+      <div>
+      <h1>Contact</h1>
+      {product ? (
+        <div>
+          <h2>{product.title}</h2>
+          <p>{product.body}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+    )
 }
